31798a5e9520c27a196d0f54bbee31ab2e018ba3
Billy Rotich <rawteech@Billys-MacBook-Pro.local>
2022-03-25 07:12:28 +0100

    Add initial serializers

--- home/api/v1/serializers.py
+++ home/api/v1/serializers.py
@@ -9,6 +9,8 @@ from allauth.account.utils import setup_user_email
 from rest_framework import serializers
 from rest_auth.serializers import PasswordResetSerializer
 
+from home.models import App, Plan, Subscription
+
 
 User = get_user_model()
 
@@ -74,3 +76,21 @@ class UserSerializer(serializers.ModelSerializer):
 class PasswordSerializer(PasswordResetSerializer):
     """Custom serializer for rest_auth to solve reset password error"""
     password_reset_form_class = ResetPasswordForm
+
+
+class AppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = App
+        fields = "__all__"
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = "__all__"
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = "__all__"
